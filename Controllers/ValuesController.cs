5cbe1a287cac9049fb40576a4d8bd0e9fd2df542
v_mnogoletov <v_mnogoletov@mail.ru>
2019-07-10 22:41:34 +0300

    Удалено лишнее

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace pw_marathon_bot.Controllers
 		[HttpGet]
 		public ActionResult<IEnumerable<string>> Get()
 		{
-			return new string[] { "value1", "value2" };
+			return new string[] { "value1", "value3" };
 		}
 
 		// GET api/values/5
--- Program.cs
+++ Program.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.IO;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace pw_marathon_bot
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
-			var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-			var host = new WebHostBuilder()
-				.UseKestrel()
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.UseConfiguration(config)
-				.UseIISIntegration()
-				.UseStartup<Startup>()
-				.Build();
+		public static void Main(string[] args) =>
+			CreateWebHostBuilder(args).Build().Run();
 
-			host.Run();
-		}
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
 	}
 }
