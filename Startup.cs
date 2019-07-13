774b90c9121a215f5866fa93625f07da0f20d42b
v_mnogoletov <v_mnogoletov@mail.ru>
2019-07-13 18:50:58 +0300

    add using

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Marathon
 {
