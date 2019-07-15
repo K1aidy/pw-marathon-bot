395438642134555adac52ce82fe651cb9f736dd5
v_mnogoletov <v_mnogoletov@mail.ru>
2019-07-15 17:51:29 +0300

    Уменбшил буфер

--- Services/Implements/ParserService.cs
+++ Services/Implements/ParserService.cs
@@ -2,7 +2,6 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using HtmlAgilityPack;
@@ -55,13 +54,11 @@ namespace Marathon.Services.Implements
 		{
 			using (var client = new HttpClient())
 			{
-				var userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Downloader/15100 MailRuGameCenter/1510 Safari/537.36";
-
 				var content = new StringContent($"<?xml version=\"1.0\" encoding=\"UTF-8\"?><MrPage2 SessionKey=\"{access}\" Page=\"https://games.mail.ru/gamecenter/sdc/\"/>");
 
 				content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
 
-				client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+				client.DefaultRequestHeaders.Add("User-Agent", _userAgent);
 
 				var response = await client.PostAsync(
 					"https://authdl.mail.ru/ec.php?hint=MrPage2",
@@ -80,13 +77,11 @@ namespace Marathon.Services.Implements
 
 		private async Task<string> GetMpop(string location)
 		{
-			var userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Downloader/15100 MailRuGameCenter/1510 Safari/537.36";
-
 			using (var handler = new HttpClientHandler())
 			using (var client = new HttpClient(handler))
 			{
 				var response = await client.GetAsync(location);
-				client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+				client.DefaultRequestHeaders.Add("User-Agent", _userAgent);
 
 				response.EnsureSuccessStatusCode();
 
@@ -97,29 +92,23 @@ namespace Marathon.Services.Implements
 		}
 		private async Task<string> GetMaraphonInfo(string mpop)
 		{
-			try
+			using (var handler = new HttpClientHandler { MaxRequestContentBufferSize = 100000 })
+			using (var client = new HttpClient(handler))
 			{
-				using (var handler = new HttpClientHandler())
-				using (var client = new HttpClient(handler))
-				{
-					handler.CookieContainer.Add(
-						new System.Uri("https://pw.mail.ru:443/supermarathon.php"),
-						new Cookie("Mpop", mpop));
+				handler.CookieContainer.Add(
+				new System.Uri("https://pw.mail.ru/supermarathon.php"),
+				new Cookie("Mpop", mpop));
 
-					client.DefaultRequestHeaders.Add("User-Agent", _userAgent);
+				client.DefaultRequestHeaders.Add("User-Agent", _userAgent);
 
-					var response = await client.GetByteArrayAsync("https://pw.mail.ru:443/supermarathon.php");
+				var response = await client.GetAsync("https://pw.mail.ru/supermarathon.php");
 
-					var text = Encoding.UTF8.GetString(response);
+				response.EnsureSuccessStatusCode();
 
-					var doc = new HtmlDocument();
-					doc.LoadHtml(text);
-					return doc.GetElementbyId("content_body").InnerHtml;
-				}
-			}
-			catch (System.Exception ex)
-			{
-				return ex.Message;
+				var html = await response.Content.ReadAsStringAsync();
+				var doc = new HtmlDocument();
+				doc.LoadHtml(html);
+				return doc.GetElementbyId("content_body").InnerHtml;
 			}
 		}
 	}
